98dbc514f839a6fb12c0fcf0053ff4e500adbc95
DINAAR <ramadaniainur2@gmail.com>
2025-11-23 23:43:00 +0700

    first commit

---
+++ app/src/main/java/pnm/tigad/dessertclicker/model/Dessert.kt
@@ -0,0 +1,7 @@
+package pnm.tigad.dessertclicker.model
+
+
+/**
+ * [Dessert] is the data class to represent the Dessert imageId, price, and startProductionAmount
+ */
+data class Dessert(val imageId: Int, val price: Int, val startProductionAmount: Int)
